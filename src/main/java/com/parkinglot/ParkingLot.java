e5e85dceada0f2a55a2ad5b5761860d8fad8eded
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-18 14:02:04 +0800

    feat: added functionality to fetch one car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,7 +1,14 @@
 package com.parkinglot;
 
 public class ParkingLot {
+    private Car car;
+
     public ParkingTicket park(Car car) {
+        this.car = car;
         return new ParkingTicket();
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return car;
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -17,4 +17,18 @@ public class ParkingLotTest {
         //then
         Assertions.assertNotNull(parkingTicket);
     }
+
+    @Test
+    void should_return_the_parked_car_when_fetch_given_parkingLot_and_parking_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //when
+        ParkingTicket parkingTicket = parkingLot.park(car);
+        Car fetchedCar = parkingLot.fetch(parkingTicket);
+
+        //then
+        Assertions.assertEquals(car, fetchedCar);
+    }
 }
