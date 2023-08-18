a9496a3aa8ce06ad47aaaa20b74e44fb8a62262b
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-18 19:26:01 +0800

    test: added test case for parking and fetching multiple cars

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -34,4 +34,23 @@ public class StandardParkingBoyTest {
         //then
         Assertions.assertEquals(car, fetchedCar);
     }
+
+    @Test
+    void should_return_the_right_car_when_fetch_car_twice_given_parkingLot_with_two_cars_standardParkingBoy_and_parkingTicket() {
+        //given
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        //when
+        ParkingTicket firstParkingTicket = StandardParkingBoy.park(parkingLot, firstCar);
+        ParkingTicket secondParkingTicket = StandardParkingBoy.park(parkingLot, secondCar);
+        Car firstFetchedCar = StandardParkingBoy.fetch(parkingLot, firstParkingTicket);
+        Car secondFetchedCar = StandardParkingBoy.fetch(parkingLot, secondParkingTicket);
+
+        //then
+        Assertions.assertEquals(firstCar, firstFetchedCar);
+        Assertions.assertEquals(secondCar, secondFetchedCar);
+    }
 }
