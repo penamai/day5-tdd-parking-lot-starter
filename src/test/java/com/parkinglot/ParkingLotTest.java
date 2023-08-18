9c2e5c80cb9c084591f20071cea435cf93c1c930
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-18 13:57:56 +0800

    test: renamed the test case

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -6,14 +6,13 @@ import org.junit.jupiter.api.Test;
 public class ParkingLotTest {
 
     @Test
-    void should_return_a_parking_ticket_when_park_given_parking_lot_and_car() {
+    void should_return_a_parking_ticket_when_park_given_parkingLot_and_car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
-        ParkingTicket parkingTicket;
 
         //when
-        parkingTicket = parkingLot.park(car);
+        ParkingTicket parkingTicket = parkingLot.park(car);
 
         //then
         Assertions.assertNotNull(parkingTicket);
