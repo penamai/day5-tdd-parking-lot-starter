e45b001f2c50cc61f7ed0346a7ef26099cfc1baf
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-20 11:14:49 +0800

    test: remove tests that are redundant

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -28,71 +28,6 @@ public class StandardParkingBoyTest {
         car = new Car();
         secondCar = new Car();
     }
-    @Test
-    void should_return_parkingTicket_when_park_given_standardParkingBoy_parkingLot_and_car() {
-        ParkingTicket parkingTicket = standardParkingBoy.park(car);
-        Assertions.assertNotNull(parkingTicket);
-    }
-
-    @Test
-    void should_return_parkedCar_when_fetch_given_standardParkingBoy_parkingLot_and_parkingTicket() {
-        ParkingTicket parkingTicket = standardParkingBoy.park(car);
-        Car fetchedCar = standardParkingBoy.fetch(parkingTicket);
-
-        Assertions.assertEquals(car, fetchedCar);
-    }
-
-    @Test
-    void should_return_the_right_car_when_fetch_car_twice_given_parkingLot_with_two_cars_standardParkingBoy_and_parkingTicket() {
-        Car firstCar = car;
-        Car secondCar = new Car();
-
-        ParkingTicket firstParkingTicket = standardParkingBoy.park(firstCar);
-        ParkingTicket secondParkingTicket = standardParkingBoy.park(secondCar);
-
-        Car firstFetchedCar = standardParkingBoy.fetch(firstParkingTicket);
-        Car secondFetchedCar = standardParkingBoy.fetch(secondParkingTicket);
-
-        Assertions.assertEquals(firstCar, firstFetchedCar);
-        Assertions.assertEquals(secondCar, secondFetchedCar);
-    }
-
-    @Test
-    void should_return_UnrecognizedTicketException_when_fetch_given_standardParkingBoy_and_two_parking_lots_and_unrecognized_ticket() {
-        ParkingTicket wrongParkingTicket = new ParkingTicket();
-        standardParkingBoy.park(car);
-
-        UnrecognizedTicketException unrecognizedTicketException = assertThrows(UnrecognizedTicketException.class,
-                () -> standardParkingBoy.fetch(wrongParkingTicket));
-
-        Assertions.assertEquals("Unrecognized parking ticket.", unrecognizedTicketException.getMessage());
-    }
-
-    @Test
-    void should_return_UnrecognizedTicketException_when_fetch_given_standardParkingBoy_and_two_parking_lots_and_used_ticket() {
-        ParkingTicket parkingTicket = standardParkingBoy.park(car);
-        standardParkingBoy.fetch(parkingTicket);
-
-        UnrecognizedTicketException unrecognizedTicketException = assertThrows(UnrecognizedTicketException.class,
-                () -> standardParkingBoy.fetch(parkingTicket));
-
-        Assertions.assertEquals("Unrecognized parking ticket.", unrecognizedTicketException.getMessage());
-    }
-
-    @Test
-    void should_return_noAvailablePositionException_when_park_given_standardParkingBoy_car_and_maxed_out_parkingLot() {
-        parkingLot1 = new ParkingLot(1);
-        managedParkingLot = List.of(parkingLot1);
-        standardParkingBoy = new StandardParkingBoy(managedParkingLot);
-        Car nextCar = new Car();
-
-        standardParkingBoy.park(car);
-
-        NoAvailablePositionException noAvailablePositionException = assertThrows(NoAvailablePositionException.class,
-                () -> standardParkingBoy.park( nextCar));
-
-        Assertions.assertEquals("No available position.", noAvailablePositionException.getMessage());
-    }
 
     @Test
     void should_put_car_in_first_parking_lot_when_park_given_standardParkingBoy_two_parkingLots_and_car() {
@@ -127,6 +62,28 @@ public class StandardParkingBoyTest {
 
         Assertions.assertEquals(car, fetchedFirstCar);
         Assertions.assertEquals(secondCar, fetchedSecondCar);
+    }
+
+    @Test
+    void should_return_UnrecognizedTicketException_when_fetch_given_standardParkingBoy_and_two_parking_lots_and_unrecognized_ticket() {
+        ParkingTicket wrongParkingTicket = new ParkingTicket();
+        standardParkingBoy.park(car);
+
+        UnrecognizedTicketException unrecognizedTicketException = assertThrows(UnrecognizedTicketException.class,
+                () -> standardParkingBoy.fetch(wrongParkingTicket));
+
+        Assertions.assertEquals("Unrecognized parking ticket.", unrecognizedTicketException.getMessage());
+    }
+
+    @Test
+    void should_return_UnrecognizedTicketException_when_fetch_given_standardParkingBoy_and_two_parking_lots_and_used_ticket() {
+        ParkingTicket parkingTicket = standardParkingBoy.park(car);
+        standardParkingBoy.fetch(parkingTicket);
+
+        UnrecognizedTicketException unrecognizedTicketException = assertThrows(UnrecognizedTicketException.class,
+                () -> standardParkingBoy.fetch(parkingTicket));
+
+        Assertions.assertEquals("Unrecognized parking ticket.", unrecognizedTicketException.getMessage());
     }
 
     @Test
